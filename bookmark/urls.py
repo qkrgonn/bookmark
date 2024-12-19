e0f782ed8f325bc5efc7b2d3c9941291e8561a9f
qkrgonn <gounpark7475@gmail.com>
2024-12-19 17:30:54 +0900

    add static_root

--- bookmark/urls.py
+++ bookmark/urls.py
@@ -10,5 +10,4 @@ urlpatterns = [
     path('detail/<int:pk>/', BookmarkDetailView.as_view(), name='detail'),
     path('update/<int:pk>/', BookmarkUpdateView.as_view(), name='update'),
     path('delete/<int:pk>/', BookmarkDeleteView.as_view(), name='delete'),
-    path('admin/', admin.site.urls),
 ]
